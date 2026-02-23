59e3a9efb265a40b92195681d7344e2a9b99470f
shuchenliu <shuchenliu2014@gmail.com>
2026-02-22 23:56:16 -0800

    config

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,9 +0,0 @@
-<!-- Note: Please must use one of our issue templates to file an issue! 🛑 -->
-<!-- 👉 https://github.com//dogpark-data-ops/issues/new/choose 👈 -->
-<!-- **Issues that should have been filed with a template will be closed without action, and we will ask you to use a template.** -->
-
-<!-- This blank issue template is only for issues that don't fit any of the templates. -->
-
-## Overview
-
-...
--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,22 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-	{ ignores: ["lib", "node_modules", "pnpm-lock.yaml"] },
-	{ linterOptions: {"reportUnusedDisableDirectives":"error"} },
-	eslint.configs.recommended,
-	{ extends: [tseslint.configs.strictTypeChecked, tseslint.configs.stylisticTypeChecked], files: ["**/*.{js,ts}"], languageOptions: {"parserOptions":{"projectService":{"allowDefaultProject":["*.config.*s"]}}}, }
+    { ignores: ["lib", "node_modules", "pnpm-lock.yaml"] },
+    { linterOptions: { reportUnusedDisableDirectives: "error" } },
+    eslint.configs.recommended,
+    {
+        extends: [
+            tseslint.configs.strictTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+        ],
+        files: ["**/*.{js,ts}"],
+        languageOptions: {
+            parserOptions: {
+                projectService: { allowDefaultProject: ["*.config.*s"] },
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+        },
+    },
 );
